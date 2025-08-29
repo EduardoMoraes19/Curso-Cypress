6a76916eaa0e78c0ddd1a0de481de4a8ce32110a
CAPZINOVA <131202382+EduardoMoraes19@users.noreply.github.com>
2025-08-28 21:26:07 -0300

    feat/test-inputs

--- cypress/e2e/ticket.spec-copy-1.cy.js
+++ cypress/e2e/ticket.spec-copy-1.cy.js
@@ -1,8 +1,16 @@
 describe("Tickets", () => {
-  it("should display the 'TICKETBOX' heading", () => {
+  it.only("fills all the text inpu fields", () => {
     // Abre a página
     cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html");
 
-    cy.get("h1").should("contain.text", "TICKETBOX");
+    const firstName = "Eduardo";
+    const lastName = "Moraes";
+    const email = "eduardomoraes19@gmail.com";
+
+    cy.get("#first-name").type(firstName);
+    cy.get("#last-name").type(lastName);
+    cy.get("#email").type(email);
+    cy.get("#requests").type("Social midia");
+    cy.get("#signature").type(`${firstName} ${lastName}`);
   });
 });
